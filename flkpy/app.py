ce0fe84d8ba90240826e536ba2bcfacf5f26303c
Ashish Waghmare <ashishwaghmare@gmail.com>
2018-05-06 12:54:29 +0530

    fix python hello world

--- flkpy/app.py
+++ flkpy/app.py
@@ -1,9 +1,9 @@
-From flask import Flask
+from flask import Flask
 app = Flask(__name__)
-	
+
 @app.route('/')
 def hello_world():
-	return 'Flask Dockerized'
+    return 'Flask: Hello World from Docker'
 
 if __name__ == '__main__':
-	app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
